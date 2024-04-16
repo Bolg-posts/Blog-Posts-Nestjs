d26f18a8aeaa9a0160de6276692336feba8ccead
yasmeen10 <yasmeenwaleed844@gmail.com>
2024-04-16 17:04:57 +0200

    newUpdate

--- src/endpoints/user/userSchema.ts
+++ src/endpoints/user/userSchema.ts
@@ -4,5 +4,6 @@ export let userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  image: String,
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Posts' }],
 });
